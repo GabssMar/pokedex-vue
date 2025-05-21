db67ef940b11bc7a9427bd32f9e419bc902e1bc4
Gabriele Martinez <gabimarti099@hotmail.com>
2025-05-20 22:35:22 -0300

    Adição de novos arquivos

---
+++ src/components/CardPokemon.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'CardPokemon',
+  props: {
+    pokemon: Object
+  },
+  methods: {
+    getImageUrl() {
+      const id = this.pokemon.url.split("/").filter(Boolean).pop()
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+    }
+  }
+}
+</script>
+
+<template>
+  <b-card class="mb-3 text-center d-flex" :title="pokemon.name">
+    <b-img :src=getImageUrl() height="120px;"/>
+    <b-button variant="primary">
+      Ver Detalhes
+    </b-button>
+  </b-card>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/PokemonListView.vue
@@ -0,0 +1,51 @@
+<script>
+import CardPokemon from '@/components/CardPokemon.vue';
+export default {
+  name: 'PokemonListView',
+  components: {CardPokemon},
+  data() {
+    return {
+      limit: 20,
+      offset: 0,
+      pokemons: [],
+      total: 0,
+      currentPage: 1
+    }
+  },
+  watch: {
+    currentPage(newCurrentPage) {
+      this.offset = (newCurrentPage - 1 ) * this.limit
+    }
+  },
+  mounted() {
+    this.fetchPokemons()
+  },
+  methods: {
+    fetchPokemons() {
+      const url = `https://pokeapi.co/api/v2/pokemon?limit={this.limit}&offset={this.offset}`
+      fetch(url).then(res => res.json())
+        .then(data => {
+          console.log(data)
+          this.pokemons = data.results
+          this.total = data.count
+          console.log(this.pokemons)
+        })
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container d-flex flex-column align-items-center">
+    <b-row class="justify-content-center w-100">
+      <b-col cols="12" md="4" lg="3" v-for="pokemon in pokemons" :key="pokemon.name">
+        <card-pokemon :pokemon="pokemon"/>
+      </b-col>
+    </b-row>
+    <b-pagination v-model="currentPage" :per-page="limit" :total-rows="total" align="center"/>
+  </div>
+</template>
+
+<style>
+
+</style>
